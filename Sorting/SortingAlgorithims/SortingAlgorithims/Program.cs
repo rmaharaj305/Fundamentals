135422ff9d3b384a335c7cb9ce5e49a398161b8f
Rahul Maharaj <44448055+rmaharaj305@users.noreply.github.com>
2022-09-19 10:26:33 -0400

    Add sorting folder

---
+++ Sorting/SortingAlgorithims/SortingAlgorithims/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SortingAlgorithims
+{
+    class Program
+    {
+        public static int[] BubbleSort(int[] heights)
+        {
+            for(int i=0; i<heights.Length-1; i++)
+            {
+                for(int j = 0; j < heights.Length-1; j++)
+                {
+                    if(heights[j] > heights[j+1])
+                    {
+                        var temp = heights[j];
+                        heights[j] = heights[j + 1];
+                        heights[j + 1] = temp;
+                    }
+                }
+            }
+            return heights;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0 };
+            int[] floydsTest = { 1, 3, 4, 2, 2 };
+            //var sortedNumbers = BubbleSort(numbers);
+            Console.WriteLine(String.Join(" ", BubbleSort(numbers)));
+
+        }
+    }
+}
