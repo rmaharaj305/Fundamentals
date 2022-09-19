a9c7b125ebaa30040d5a206146f62709bb7f5285
Rahul Maharaj <44448055+rmaharaj305@users.noreply.github.com>
2022-09-19 10:27:43 -0400

    Add files via upload

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        public static int BinarySearch(int[] numbers, int target)
+        {
+            int min = 0;
+            int max = numbers.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (target == numbers[mid])
+                {
+                    return mid;
+                }
+                else if (target < numbers[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 6, 7, 8, 9, 10,11};
+            int target = 1;
+            Console.WriteLine(String.Join(" ",BinarySearch(numbers,target)));
+        }
+    }
+}
